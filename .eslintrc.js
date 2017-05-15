3626e6b0563459f05ae22f9cf089f3bf1bae3388
Валентин Немцев <v-nemcev@yandex-team.ru>
2017-05-15 20:21:57 +0300

    Update to use light-dom

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: 'qe-frontend',
+    parser: 'babel-eslint',
+    settings: {
+        // npm install -DE eslint-import-resolver-node
+        'import/resolver': 'node'
+    }
+}
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import assert from 'assert';
-
 import ace from 'brace';
 import 'brace/theme/clouds';
 import 'brace/mode/json';
@@ -22,87 +20,79 @@ const aceEditorModule = {
         'transposeletters',
     ],
 
-    create: (oldVnode, vnode) => {
-        if (!vnode.data.aceEditor) return;
-        const {mode, options = {}} = vnode.data.aceEditor;
-
-        const elm = vnode.elm;
-        const aceEl = document.createElement('div');
-        const editor = ace.edit(aceEl);
-        editor.$blockScrolling = Infinity; // Disable warning
-        editor.setTheme('ace/theme/clouds');
-        editor.setShowPrintMargin(false);
-        editor.setOptions({
-            minLines: 3,
-            maxLines: Infinity,
-            ...options,
-        });
-
-        aceEditorModule.commandsBlacklist
-            .forEach(c => editor.commands.removeCommand(c));
-
-        const session = editor.getSession();
-        session.setMode('ace/mode/' + mode);
-        session.setUseWrapMode(true);
-        session.on(
-            'change',
-            () => {
-                // Change was initiated by user, not API call
-                if (editor.curOp && editor.curOp.command.name) {
-                    elm.value = editor.getValue();
-                    // For snabbdom native event listeners, no IE <=11 support
-                    elm.dispatchEvent(new Event('change', {bubbles: true}));
-                }
-            }
-        );
-
-        aceEditorModule._editors.set(elm, editor);
-
-        // Defer initial update for better perceived performance when text is
-        // long
-        setTimeout(() => aceEditorModule._updateValue(editor, vnode), 1);
-
-        const hook = vnode.data.hook = vnode.data.hook || {};
-        assert(
-            hook.insert == null,
-            'Insert hook already exists for ' + vnode.sel
-        );
-        hook.insert = () => {
-            elm.hidden = true;
-            elm.after(aceEl);
-        };
-    },
 
     _updateValue(editor, vnode) {
-        vnode.elm.value = vnode.text;
+        const text = vnode.children[0].text;
+        if (editor.getValue() === text) return;
+        vnode.elm.value = text;
         const selection = editor.session.selection.toJSON();
-        editor.setValue(vnode.text != null ? vnode.text : '', -1);
+        editor.setValue(text != null ? text : '', -1);
         editor.session.selection.fromJSON(selection);
     },
 
-    update: (oldVnode, vnode) => {
-        if (!vnode.data.aceEditor) return;
-        assert.deepEqual(
-            oldVnode.data.aceEditor,
-            vnode.data.aceEditor,
-            'aceEditor changed for ' + vnode.sel
-        );
-
-        if (oldVnode.elm.value !== vnode.text) {
-            const editor = aceEditorModule._editors.get(vnode.elm);
-            aceEditorModule._updateValue(editor, vnode);
-        }
-    },
-
-    destroy: (vnode) => {
-        if (!vnode.data.aceEditor) return;
-
-        const hook = vnode.data.hook || {};
-        delete hook.insert;
-        const editor = aceEditorModule._editors.get(vnode.elm);
-        editor.destroy();
-        editor.container.remove();
-    },
 };
 
-export default aceEditorModule;
+export default function (aceEditor) {
+    return {
+        insert(vnode) {
+            const elm = vnode.elm;
+            if (elm.hasAceEditor) return;
+
+            const {setStyle, mode, options = {}} = aceEditor;
+
+            const aceEl = document.createElement('div');
+            const editor = ace.edit(aceEl);
+            editor.$blockScrolling = Infinity; // Disable warning
+            editor.setTheme('ace/theme/clouds');
+            editor.setShowPrintMargin(false);
+            editor.setStyle(setStyle);
+            editor.setOptions({
+                minLines: 3,
+                maxLines: Infinity,
+                ...options,
+            });
+
+            aceEditorModule.commandsBlacklist
+                .forEach(c => editor.commands.removeCommand(c));
+
+            const session = editor.getSession();
+            session.setMode('ace/mode/' + mode);
+            session.setUseWrapMode(true);
+            session.on(
+                'change',
+                () => {
+                    // Change was initiated by user, not API call
+                    if (editor.curOp && editor.curOp.command.name) {
+                        elm.value = editor.getValue();
+                        // For snabbdom native event listeners, no IE <=11 support
+                        elm.dispatchEvent(new Event('change', {bubbles: true}));
+                    }
+                }
+            );
+
+            aceEditorModule._editors.set(elm, editor);
+
+            // Defer initial update for better perceived performance when text is
+            // long
+            setTimeout(() => aceEditorModule._updateValue(editor, vnode), 1);
+
+            elm.hasAceEditor = true;
+            elm.hidden = true;
+            elm.after(aceEl);
+        },
+
+        update: (oldVnode, vnode) => {
+            const editor = aceEditorModule._editors.get(vnode.elm);
+            editor && aceEditorModule._updateValue(editor, vnode);
+        },
+
+        destroy: (vnode) => {
+
+            const hook = vnode.data.hook || {};
+            delete hook.insert;
+            const editor = aceEditorModule._editors.get(vnode.elm);
+            editor.destroy();
+            editor.container.remove();
+        },
+    };
+}
